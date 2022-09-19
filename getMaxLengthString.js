6ccf39215d679d9d37326f7e8792328916f5c0ad
PCJR27 <88100488+PCJR27@users.noreply.github.com>
2022-09-19 19:08:32 +0700

    Update2

--- getMaxLengthString.js
+++ getMaxLengthString.js
@@ -1,9 +1,30 @@
 const { template } = require('@babel/core')
 
 function getMaxLengthString(arrayOfString) {
-if(arrayOfString === null || arrayOfString === undefined){
-  return undefined
-}
 
+ 
+  if(arrayOfString === null || arrayOfString === undefined){
+    return undefined
+  }
+  let answerMax =[]
+  let answer =arrayOfString[0]
+  for (const values of arrayOfString){
+    if (answerMax.length === 0){
+     answerMax = [values]
+    }
+    else if (values.length >= answerMax[0].length){
+     if(values.length == answerMax[0].length){
+       answerMax.push(values)
+     }
+     else{
+       answerMax = [values] 
+     }
+    }
+  }
+    return answerMax
+  
 }
+console.log(getMaxLengthString(['hello', 'hi', 'hey', 'Good Evening', 'Good Morning','Good Mornind']))
+console.log(getMaxLengthString(['hello','hi']))
 module.exports = getMaxLengthString
+
